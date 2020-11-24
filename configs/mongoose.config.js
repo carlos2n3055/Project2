98d3f553e0c53d8690e00264bc66544f98c5f05a
Carlos Martin-Salas Larena <carlos_2n3055@hotmail.com>
2020-11-24 17:17:09 +0100

    done

--- configs/mongoose.config.js
+++ configs/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb+srv://carlosmartinsalaslarena:S281281c@cluster0.i1nyl.mongodb.net/shopsInt', { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.DB_REMOTE, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
     .catch(err => console.error('Error connecting to mongo', err))
 
