839c970531e87dd5065fea645640e3de7776a42c
Carlos Martin-Salas Larena <carlos_2n3055@hotmail.com>
2020-11-27 11:55:20 +0100

    refactoring

--- middlewares/custom.middlewares.js
+++
@@ -1,5 +0,0 @@
-const ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login', { errorMsg: 'Not authorized, please log in' })
-
-const checkRole = admittedRoles => (req, res, next) => admittedRoles.includes(req.user.role) ? next() : res.render('auth/login', { errorMsg: 'Not authorized, you need a permit' })
-
-// const checkRoleAdmin = (req, res, next) => req.user.role === "ADMIN" ? next() : res.render('auth/login', { errorMsg: 'Not authorized, you need a permit' })
--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -4,9 +4,6 @@ const Shop = require('./../models/shop-model')
 
 const CDNUpload = require('./../configs/cdn-upload.config')  // Cloudinary
 
-// const { ensureAuthenticated, checkRoleAdmin } = require('./../middlewares/custom.middlewares.js')
-// const { ensureAuthenticated, checkRole } = require('./../middlewares/custom.middlewares.js')
-
 const ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login', { errorMsg: 'Not authorized, please log in' })
 const checkRole = admittedRoles => (req, res, next) => admittedRoles.includes(req.user.role) ? next() : res.render('auth/login', { errorMsg: 'Not authorized, you need a permit' })
 
@@ -34,8 +31,6 @@ router.get('/', (req, res, next) => {
 // ----- NEW SHOP -----
 
 // Muestra el formulario para crear una tienda (GET)
-// router.get('/new', ensureAuthenticated, checkRoleAdmin, (req, res) => res.render('shop/shop-new'))
-
 router.get('/new', ensureAuthenticated, checkRole(['ADMIN', 'OWNER']), (req, res) => res.render('shop/shop-new'))
 
 
@@ -88,9 +83,7 @@ router.post('/new', CDNUpload.single("imageFile"), (req, res, next) => {
 // ----- DELETE SHOP -----
 
 // Elimina de la BBDD la tienda (GET)
-// router.get('/delete', ensureAuthenticated, checkRoleAdmin, (req, res, next) => {
-
-  router.get('/delete', ensureAuthenticated, checkRole(['ADMIN']), (req, res, next) => {
+router.get('/delete', ensureAuthenticated, checkRole(['ADMIN']), (req, res, next) => {
 
     const shopId = req.query.id
 
@@ -104,9 +97,7 @@ router.post('/new', CDNUpload.single("imageFile"), (req, res, next) => {
 // ----- EDIT SHOP -----
 
 // Muestra el formulario para editar una tienda (GET)
-// router.get('/edit', ensureAuthenticated, checkRoleAdmin, (req, res, next) => {
-
-  router.get('/edit', ensureAuthenticated, checkRole(['ADMIN']), (req, res, next) => {
+router.get('/edit', ensureAuthenticated, checkRole(['ADMIN']), (req, res, next) => {
 
   const shopId = req.query.id
 
